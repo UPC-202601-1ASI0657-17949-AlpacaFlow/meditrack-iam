08d58cbd76c1c81fc7cba75da750e7ee79c51866
RicardoCardenas <fernandocm1112@gmail.com>
2026-05-28 19:20:19 -0500

    Refactor changed URL Organization

--- src/main/java/com/alpacaflow/meditrack/iam/iam/infrastructure/acl/client/OrganizationClient.java
+++ src/main/java/com/alpacaflow/meditrack/iam/iam/infrastructure/acl/client/OrganizationClient.java
@@ -1,10 +1,8 @@
 package com.alpacaflow.meditrack.iam.iam.infrastructure.acl.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 // "organization-service" es el nombre del microservicio o su URL configurada en Docker Compose
 @FeignClient(name = "organization-service", url = "${organization.facade.url}")
