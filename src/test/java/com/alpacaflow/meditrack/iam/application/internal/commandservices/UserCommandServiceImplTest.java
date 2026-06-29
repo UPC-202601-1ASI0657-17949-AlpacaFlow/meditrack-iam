9456b9cc47475fcb5d718d230a5fa3bf49225a30
fredyst22 <caaarlos1128@gmail.com>
2026-06-29 16:39:39 -0500

    fix(iam): align local Feign facade URLs with gateway defaults
    Use http://localhost:8282 for organization and add relatives facade URL for local development.

--- src/test/java/com/alpacaflow/meditrack/iam/application/internal/commandservices/UserCommandServiceImplTest.java
+++ src/test/java/com/alpacaflow/meditrack/iam/application/internal/commandservices/UserCommandServiceImplTest.java
@@ -3,6 +3,7 @@ package com.alpacaflow.meditrack.iam.application.internal.commandservices;
 import com.alpacaflow.meditrack.iam.iam.application.internal.commandservices.UserCommandServiceImpl;
 import com.alpacaflow.meditrack.iam.iam.application.internal.outboundservices.acl.OrganizationContextFacade;
 import com.alpacaflow.meditrack.iam.iam.application.internal.outboundservices.acl.RelativesContextFacade;
+import com.alpacaflow.meditrack.iam.iam.application.internal.outboundservices.hashing.HashingService;
 import com.alpacaflow.meditrack.iam.iam.application.internal.outboundservices.tokens.TokenService;
 import com.alpacaflow.meditrack.iam.iam.domain.model.aggregates.User;
 import com.alpacaflow.meditrack.iam.iam.domain.model.commands.CreateMockUserCommand;
